e3a19321aac6f3b3ae2f860e07a2ca069171a638
Abhay Kulkarni <developer.ak23@gmail.com>
2017-06-24 22:11:56 +0530

    basic constants and for loop

---
+++ src/04constants.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+import "math"
+
+// const declares a constant value
+const s string = "constant"
+
+func main() {
+  fmt.Println(s)
+  const n = 500000000
+  // const expressions perform arithmetic with arbitrary precision
+  const d = 3e20 / n
+  fmt.Println(d)
+
+  // a numeric constant has no type until it's given one such by an
+  // explitcit cast
+  fmt.Println(int64(d))
+
+  // a number can be given a type by using it in a context, for 
+  // instance a variable assignment or a function call.
+  // Here math.Sin expects a float64.
+  fmt.Println(math.Sin(n))
+}
