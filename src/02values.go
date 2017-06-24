1b3fd968b2cc16e26ade4c0823f8628ed046248d
Abhay Kulkarni <developer.ak23@gmail.com>
2017-06-24 21:26:19 +0530

    beginning to learn Go

---
+++ src/02values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+  fmt.Println("go" + "lang")
+  fmt.Println("1+1 = ",  1+1)
+  fmt.Println("(1+1) again with brackets  = ", (1+1))
+  fmt.Println(true && false)
+  fmt.Println(true || false)
+  fmt.Println(!true)
+  //fmt.Println(true & false) & operator is not defined on booleans
+}
---
+++ src/03variables.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+  // var declares one or more variables
+  var a string = "initial"
+  fmt.Println(a)
+
+  // multiple variables can be declared in a single line
+  var b,c int = 1, 2
+  fmt.Println(b,c)
+
+  // go will infer the type of variables
+  var d = true
+  fmt.Println(d)
+
+  // variables without initialization are defaulted to 0
+  var e int
+  fmt.Println(e)
+
+  // := shorthand for declaring and initializing
+  f := "short"
+  fmt.Println(f)
+}
