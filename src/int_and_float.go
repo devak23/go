2d23294a844efb03c031ad17401303aa4e93cd70
Abhay Kulkarni <developer.ak23@gmail.com>
2017-07-12 15:49:02 +0530

    Refactored code

--- src/int_and_float.go
+++ src/int_and_float.go
@@ -10,14 +10,14 @@ func main() {
 	var k float64 = 3.14159
 	var m int = 2
 	var result1 = int(k) + m
-	fmt.Println("result1 = ", result1)
 
 	var result2 int = int(k) * m
-	fmt.Println("result2 = ", result2)
+	fmt.Printf("%T %T\n", result1, result2)
+	fmt.Printf("%v %v\n", result1, result2)
 
 	var result3 float64 = k + float64(m)
-	fmt.Println("result3 = ", result3)
-
 	var result4 float64 = k * float64(m)
-	fmt.Println("result4 = ", result4)
+
+	fmt.Printf("%F %F\n", result3, result4)
+	fmt.Printf("%v %v\n", result3, result4)
 }
