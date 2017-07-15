4de8d0cf4f8016dd23303cb4e37e9be736ccf7bb
Abhay Kulkarni <developer.ak23@gmail.com>
2017-07-15 22:49:18 +0530

    Added comments to the code

--- src/goinpractise/01multiple_returns.go
+++ src/goinpractise/01multiple_returns.go
@@ -5,22 +5,25 @@ import (
 	"fmt"
 )
 
-// Names returns multiple return values. This is a public function
+// MultipleReturns returns multiple return values. This is a public function
 func MultipleReturns() (string, string) {
 	return "Abhay", "Soham"
 }
 
+// MultipleReturnsWithParams accepts two string arguments and returns them
+// The example only illustrates that multiple return values can be returned
+// from the function
 func MultipleReturnsWithParams(name1 string, name2 string) (string, string) {
 	return name1, name2
 }
 
-// This is the main function called MultipleReturnsMain so as to be
+// MutlipleReturnsMain is the main function called MultipleReturnsMain so as to be
 // invoked from main.go
 func MutlipleReturnsMain() {
 	n1, n2 := MultipleReturns()
 	fmt.Println(n1, n2)
 
 	// anything with an underscore is ignored
-	n3, _ := MultipleReturns()
+	n3, _ := MultipleReturnsWithParams("Manik", "Soham")
 	fmt.Println(n3)
 }
