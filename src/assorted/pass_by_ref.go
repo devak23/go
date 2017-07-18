4469e4ae7f2617547e1db069819d8bed75573afb
Abhay Kulkarni <developer.ak23@gmail.com>
2017-07-18 11:20:27 +0530

    Added another way to define a function with reference

--- src/assorted/pass_by_ref.go
+++ src/assorted/pass_by_ref.go
@@ -2,9 +2,15 @@ package assorted
 
 import "fmt"
 
-func addOne(a *int) *int {
+func addOne(a *int) *int { // <---- note the differences here
 	*a = *a + 1
 	return a
+}
+
+// another way to define the function using references
+func addTwo(a *int) int { // <---- note the differences here
+	*a = *a + 2
+	return *a
 }
 
 // PassByRefMain is the main function invoked from main.go
@@ -14,5 +20,12 @@ func PassByRefMain() {
 
 	x1 := addOne(&x)
 	fmt.Println("x = ", x)
-	fmt.Println("x1 = ", *x1)
+	fmt.Println("x1 = ", *x1) // <---- note the differences here
+
+	y := 25
+	fmt.Println(" y =", y)
+
+	y1 := addTwo(&y)
+	fmt.Println("y1 = ", y1)
+	fmt.Println("y = ", y) // <---- note the differences here
 }
