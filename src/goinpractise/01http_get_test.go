10b63fe82bb11a6d9f8e70cdf0dbd9d4d6efc193
Abhay Kulkarni <developer.ak23@gmail.com>
2017-07-22 21:14:55 +0530

    Renamed error variable to err

--- src/goinpractise/01http_get_test.go
+++ src/goinpractise/01http_get_test.go
@@ -7,14 +7,14 @@ import (
 
 func TestHttpGetWithGoogleDotCom(t *testing.T) {
 	url := "http://www.google.com"
-	response, error := HttpGet(url)
-	assert.AssertEquals(t, error, nil, "")
+	response, err := HttpGet(url)
+	assert.AssertEquals(t, err, nil, "")
 	assert.AssertNotNil(t, response, "")
 }
 
 func TestHttpGetWithJunkWebsiteGivesError(t *testing.T) {
 	url := "http://hhhhhhh:8080/"
-	response, error := HttpGet(url)
+	response, err := HttpGet(url)
 	assert.AssertEquals(t, response, "", "")
-	assert.AssertNotNil(t, error, "")
+	assert.AssertNotNil(t, err, "")
 }
