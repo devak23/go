d17196dbc4cb00519a73b1d1218c8f1d5a3c8757
devak23 <developer.ak23@gmail.com>
2017-07-27 20:43:10 +0530

    Added an example which deals with input

--- src/main/main.go
+++ src/main/main.go
@@ -1,128 +1,57 @@
 package main
 
 import (
-	_ "assorted"
-	_ "gobyexample"
-	_ "goreceipes/concurrency"
-	"goreceipes/fundamentals"
+	//_ "assorted"
+	//_ "gobyexample"
+	//_ "goreceipes/concurrency"
+	//_ "goreceipes/fundamentals"
+	 "waytogo"
 )
 
 func main() {
-	//fmt.Println("============ Executing gobyexample.HelloMain =================")
-	//gobyexample.HelloMain()
-	// fmt.Println("============ Executing gobyexample.ArraysMain =================")
+	// gobyexample.HelloMain()
 	// gbe.ArraysMain()
-	// fmt.Println("============ Executing gobyexample.ConstantsMain =================")
 	// gbe.ConstantsMain()
-	// fmt.Println("============ Executing gobyexample.ForMain =================")
 	// gbe.ForMain()
-	// fmt.Println("============ Executing gobyexample.IfElseMain =================")
 	// gbe.IfelseMain()
-	// fmt.Println("============ Executing gobyexample.SwitchMain =================")
 	// gbe.SwitchMain()
-	// fmt.Println("============ Executing gobyexample.ValuesMain =================")
 	// gbe.ValuesMain()
-	// fmt.Println("============ Executing gobyexample.VariablesMain =================")
 	// gbe.VariablesMain()
-	//
-	// fmt.Println("============ Executing assorted.ConstantsMain =================")
 	// assorted.ConstantsMain()
-	// fmt.Println("============ Executing assorted.DoubleNumberMain =================")
 	// assorted.DoubleNumberMain()
-	// fmt.Println("============ Executing assorted.FunctionsMain =================")
 	// assorted.FunctionsMain()
-	// fmt.Println("============ Executing assorted.IntAndFloatMain =================")
 	// assorted.IntAndFloatMain()
-	// fmt.Println("============ Executing assorted.SampeStructMain =================")
 	// assorted.SampleStructMain()
-
-	// fmt.Println("============ Executing goinpractise.MutlipleReturnsMain =================")
 	// goinpractise.MutlipleReturnsMain()
-	//
-	// fmt.Println("============ Executing goinpractise.ReadStatusMain =================")
 	// goinpractise.ReadStatusMain()
-	//
-	// fmt.Println("============ Executing goinpractise.ConcurrentPrintingMain =================")
 	// goinpractise.ConcurrentPrintingMain()
-	//
-	// fmt.Println("============ Executing goinpractise.UsingChannelMain =================")
 	// goinpractise.UsingChannelMain()
-	//
-	// fmt.Println("============ Executing goinpractise.FlagCliMain =================")
 	// goinpractise.FlagCliMain()
-
-	// fmt.Println("============ Executing goinpractise.HelloServerMain =================")
 	// goinpractise.HelloServerMain()
-
-	// fmt.Println("================= Executing assorted.ConvertStringMain ===================")
 	// assorted.ConvertStringMain()
-	//
-	// fmt.Println("================= Executing assorted.MapDemoMain ===================")
 	// assorted.MapDemoMain()
-	//
-	// fmt.Println("================= Executing assorted.PassByValueMain ===================")
 	// assorted.PassByValueMain()
-	//
-	// fmt.Println("================= Executing assorted.PassByRefMain ===================")
 	// assorted.PassByRefMain()
-	//
-	// fmt.Println("================= Executing assorted.FuncAsObjMain ===================")
 	// assorted.FuncAsObjMain()
-	//
-	// fmt.Println("================= Executing assorted.AreaMain ===================")
 	// assorted.AreaMain()
-	//
-	// fmt.Println("================= Executing assorted.OOMthdInheritanceMain ===================")
 	// assorted.OOMthdInteritanceMain()
-	//
-	// fmt.Println("================= Executing assorted.OOInterfaceMain ===================")
 	// assorted.OOInterfaceMain()
-	//
-	// fmt.Println("================= Executing assorted.InterfaceValueTypeMain ===================")
 	// assorted.InterfaceValueTypeMain()
-	//
-	// fmt.Println("================= Executing assorted.GoRoutineMain ===================")
 	// assorted.GoRoutineMain()
-
-	//fmt.Println("================= Executing assorted.NonBufferedChannels ===================")
-	//assorted.NonBufferedChannels()
-	//
-	//fmt.Println("================= Executing assorted.BufferedChannelsMain ===================")
-	//assorted.BufferedChannelsMain()
-	//
-	//fmt.Println("================= Executing assorted.FiboMain ===================")
-	//assorted.FiboMain()
-
-	//fmt.Println("========== Executing assorted.ReadingDBMain ==============")
-	//assorted.ReadingDBMain()
-
-	//fmt.Println("========== Executing concurrency.WaitGroupMain ==============")
-	//concurrency.WaitGroupMain()
-
-	// fmt.Println("========== Executing concurrency.FetchUrlsMain ==============")
+	// assorted.NonBufferedChannels()
+	// assorted.BufferedChannelsMain()
+	// assorted.FiboMain()
+	// assorted.ReadingDBMain()
+	// concurrency.WaitGroupMain()
 	// assorted.FetchUrlsMain()
-
-	//fmt.Println("========== Executing assorted.CommunicationMain ==============")
-	//assorted.CommunicationMain()
-
-	//fmt.Println("========== Executing assorted.DeadlockMain ==============")
-	//assorted.DeadlockMain()
-
-	//fmt.Println("========== Executing assorted.DeadlockMain ==============")
-	//concurrency.PassingTheBuckMain()
-
-	//fmt.Println("========== Executing concurrency.PipelineMain ==============")
-	//concurrency.PipelineMain()
-
-	//fmt.Println("========== Executing concurrency.AsyncCommMain() ==============")
-	//concurrency.AsyncCommMain()
-
-	//fmt.Println("========== Executing concurrency.MultiChannelCommMain() ==============")
-	//concurrency.MultiChannelCommMain()
-
-	// fmt.Println("========== Executing fundamentals.MapIterationMain() ==============")
+	// assorted.CommunicationMain()
+	// assorted.DeadlockMain()
+	// concurrency.PassingTheBuckMain()
+	// concurrency.PipelineMain()
+	// concurrency.AsyncCommMain()
+	// concurrency.MultiChannelCommMain()
 	// fundamentals.MapIterationMain()
-
-	// fmt.Println("=========== Executing fundamentals.FileReadMain() ==================")
-	fundamentals.FileReadMain()
+	// fundamentals.FileReadMain()
+	// waytogo.ReadInputBasicMain()
+	waytogo.ReadWithBufIoMain()
 }
