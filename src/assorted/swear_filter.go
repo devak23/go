b560ada681b494df1493bd280253de387f6ebf5a
devak23 <developer.ak23@gmail.com>
2017-08-11 22:45:19 +0530

    Completed the swear word filter.

--- src/assorted/swear_filter.go
+++ src/assorted/swear_filter.go
@@ -6,8 +6,20 @@ import (
 	"os"
 	"bufio"
 	"time"
+	"strings"
 )
 
+// this function connects to redis to load the reference data
+func loadWords(conn redis.Conn) {
+	// get the curse_words from redis
+	curse_words, _ := getValuesFromRedisSet(conn, "curse_words")
+
+	if len(curse_words) == 0 {
+		addRefData(conn)
+	}
+}
+
+// This is the main functiot that gets invoked from the main.go
 func SwearFilterMain() {
 	// connect to redis server
 	conn, err := redis.Dial("tcp", "127.0.0.1:6379")
@@ -17,22 +29,63 @@ func SwearFilterMain() {
 	}
 	defer conn.Close()
 
-	// delete the user-defined words
-	conn.Do("DEL", "user_words")
+	// load the reference data
+	loadWords(conn)
 
-	// get the curse_words from redis
-	curse_words, err := getValuesFromRedisSet(conn, "curse_words")
-	fmt.Println(curse_words)
+	// start the main loop
+	for {
+		fmt.Println("Enter some sentences with the swear words. Then press Enter or \"q\" to quit")
+		bio := bufio.NewReader(os.Stdin)
+		line, _, _ := bio.ReadLine()
 
-	if len(curse_words) == 0 {
-		addWordsToSet(conn)
+		if string(line) == "q" {
+			break
+		}
+		terms := strings.Split(string(line), " ")
+		analyze(conn, terms)
 	}
 
-	//now for the main program
+	fmt.Println("Session ended.")
+}
+
+// this function analyzes the user data and tries to find the swear words in a sentence
+// it does this by creating a new key in redis called user_words. It then loads all the
+// data user entered against this key and then takes the intersection of two sets of words
+// i.e. the curse words and the user_words.
+func analyze(conn redis.Conn, userData []string) {
+	// asks redis to start a transaction as the next commands will have to be queued for atomic
+	// execution using EXEC command
+	conn.Send("MULTI")
+	// delete the temp set "user_words
+	conn.Send("DEL", "user_words")
+	// add the user_data
+	conn.Send("SADD", redis.Args{}.Add("user_words").AddFlat(userData)...)
+	// take the intersection of both sets
+	conn.Send("SINTER", "user_words", "curse_words")
+
+	reply, err := conn.Do("EXEC")
+
+	if err != nil {
+		fmt.Printf("Error executing EXEC: %s",err)
+		os.Exit(1)
+	}
+
+	values, _ := redis.Values(reply, nil)
+
+	curse_words, err := redis.Strings(values[2], nil)
+	if err != nil {
+		fmt.Printf("Error parsing the reply from redis: %s", err)
+	}
+
+	if len(curse_words) > 0 {
+		for _, word := range curse_words {
+			fmt.Println(">> Found: ", word)
+		}
+	}
 }
 
 // a function to insert words into redis using a standard data source
-func addWordsToSet(conn redis.Conn) error {
+func addRefData(conn redis.Conn) error {
 	// open the list of swear words
 	startTime := time.Now()
 	file, err := os.Open("resources/swear_words.txt")
@@ -42,9 +95,8 @@ func addWordsToSet(conn redis.Conn) error {
 	}
 	defer file.Close()
 
-
 	// create a slice variable
-	scanner:= bufio.NewScanner(file)
+	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		_, err = conn.Do("SADD", "curse_words", scanner.Text())
 	}
@@ -53,7 +105,6 @@ func addWordsToSet(conn redis.Conn) error {
 	return err
 }
 
-
 func getValuesFromRedisSet(conn redis.Conn, key string) ([]interface{}, error) {
 	values, err := redis.Values(conn.Do("SMEMBERS", key))
 	if err != nil {
@@ -61,5 +112,5 @@ func getValuesFromRedisSet(conn redis.Conn, key string) ([]interface{}, error) {
 		return nil, err
 	}
 
-	return values,nil
+	return values, nil
 }
