3454c0ca64834585366dc4d3ba3811ac4b8575cd
devak23 <developer.ak23@gmail.com>
2017-08-17 20:55:08 +0530

    Continuing with the chat application

--- src/go-blueprints/chat/main.go
+++ src/go-blueprints/chat/main.go
@@ -28,8 +28,11 @@ func (t *templateHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) 
 }
 
 func main() {
+  r := newRoom()
   http.Handle("/", &templateHandler{filename:"chat.html"})
-
+  http.Handle("/room", r)
+  // get the room going
+  go r.run()
   // start the web server
   if err := http.ListenAndServe(":8080",nil); err != nil {
     log.Fatal("ListenAndServe: ", err)
