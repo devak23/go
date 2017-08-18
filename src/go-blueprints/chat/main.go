9c95de6bf1edb13abebd64c14432ef2a7ad73569
devak23 <developer.ak23@gmail.com>
2017-08-18 08:24:21 +0530

    parameterized the chat server to accept the address information from the command line

--- src/go-blueprints/chat/main.go
+++ src/go-blueprints/chat/main.go
@@ -6,6 +6,7 @@ import (
   "sync"
   "text/template"
   "path/filepath"
+  "flag"
 )
 
 type templateHandler struct {
@@ -24,17 +25,23 @@ func (t *templateHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) 
   })
   // compiling the template in the ServeHTTP method also ensures that our code does the work
   // only when its actually needed and not before it
-  t.tmpl.Execute(res, nil)
+  t.tmpl.Execute(res, req)
 }
 
 func main() {
+  var addr = flag.String("addr", ":8080","The address of the application")
+  flag.Parse()
+
   r := newRoom()
   http.Handle("/", &templateHandler{filename:"chat.html"})
   http.Handle("/room", r)
+
   // get the room going
   go r.run()
+
   // start the web server
-  if err := http.ListenAndServe(":8080",nil); err != nil {
+  log.Println("Starting web server on", *addr)
+  if err := http.ListenAndServe(*addr,nil); err != nil {
     log.Fatal("ListenAndServe: ", err)
   }
 }
