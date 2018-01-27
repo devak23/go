6c29bc6e8c362f42de72e091fe04f2b151cb0a44
devak23 <developer.ak23@gmail.com>
2018-01-27 10:54:35 +0530

    Content-Type is now set on the header

--- lss/src/github.com/lss/webapp/main.go
+++ lss/src/github.com/lss/webapp/main.go
@@ -6,7 +6,23 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
+
+func setContentType(w http.ResponseWriter, urlPath string) {
+	var contentType string
+	switch {
+	case strings.HasSuffix(urlPath, "css"):
+		contentType = "text/css"
+	case strings.HasSuffix(urlPath, "html"):
+		contentType = "text/html"
+	case strings.HasSuffix(urlPath, "png"):
+		contentType = "image/png"
+	default:
+		contentType = "text/plain"
+	}
+	w.Header().Add("Content-Type", contentType)
+}
 
 func main() {
 	fmt.Println("A simple File server")
@@ -17,11 +33,15 @@ func main() {
 			w.WriteHeader(http.StatusInternalServerError)
 			log.Println(err)
 		} else {
+			defer f.Close()
+			// The handler also need to tell the browser what kind of file it is
+			setContentType(w, r.URL.Path)
+
 			// copy the contents of the file directly onto the Response Writer
 			// ReponseWriter implements the Writer interface, this is ok.
 			io.Copy(w, f)
+
 		}
-		defer f.Close()
 	})
 
 	fmt.Println("file server running on port 8000...")
