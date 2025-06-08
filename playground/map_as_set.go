090dd0094711689c14dfd0c8b0a85b6bf01f49f5
abhayk <developer.ak23@gmail.com>
2025-06-08 19:28:46 +0530

    added comments

--- playground/map_as_set.go
+++ playground/map_as_set.go
@@ -11,11 +11,16 @@ func (b Book) String() string {
 	return b.Title + " by " + b.Author
 }
 
-type bookCollection map[Book]struct{}
+// setOfBooks defines a new type named that is based on a map. This is a common go idiom for implementing a set (which
+// go doesn't have a built-in type). The empty struct takes zero memory, making it ideal for sets where you only care
+// about the presence or absence of an element and not the associated value. You can see that the setOfBooks uses Book
+// struct as the key. This means each book can appear only once in the set. Using struct{} as a value-type (instead of a
+// boolean or an integer) is memory-efficient since an empty struct occupies no memory.
+type setOfBooks map[Book]struct{}
 
-func MapAsSet() {
+func UsingMapAsSet() {
 	// Create a new empty book collection
-	books := make(bookCollection)
+	books := make(setOfBooks)
 
 	// Add a book to the collection
 	yesMinister := Book{Title: "Yes Minister", Author: "Anthony Jay"}
