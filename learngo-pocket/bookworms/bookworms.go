f768782e1508a6a18855692ae321cd947e0f20c8
abhayk <developer.ak23@gmail.com>
2025-06-08 13:33:59 +0530

    implemented sorting by implementing the sort.Interface

--- learngo-pocket/bookworms/bookworms.go
+++ learngo-pocket/bookworms/bookworms.go
@@ -23,6 +23,29 @@ func (b Book) String() string {
 	return fmt.Sprintf("%s: %s", b.Title, b.Author)
 }
 
+// To sort the books in the output of the main function, we used sort.Slice, which takes a function as the sorting
+// strategy. There is a second option the sort package provides sort.Interface, which can be implemented to sort slices
+// or user-defined collections. It becomes very handy when implementing custom sorting — in our case, by author and title.
+// This sorting applies only to collection so we add an intermediate custom type representing the collection of Book. The
+// following listing shows the implementation of sort.Interface with a type called byAuthorThenTitle.
+type byAuthorThenTitle []Book
+
+// Len implements sort.Interface by returning the length of the BooksByAuthor slice.
+func (b byAuthorThenTitle) Len() int { return len(b) }
+
+// Swap implements sort.Interface and swaps 2 books
+func (b byAuthorThenTitle) Swap(i, j int) {
+	b[i], b[j] = b[j], b[i]
+}
+
+// Less implements sort.Interface and returns books sorted by Author first and then by Title
+func (b byAuthorThenTitle) Less(i, j int) bool {
+	if b[i].Author != b[j].Author {
+		return b[i].Author < b[j].Author
+	}
+	return b[i].Title < b[j].Title
+}
+
 // Each Go field is tagged with the name of the JSON field. Note that the name of the field doesn’t have to match the
 // name of the tag - it’s simply a convention that makes it more readable. Another convention is that fields that are
 // slices should be named with plural word.
@@ -79,8 +102,13 @@ func findCommonBooks(bookworms []Bookworm) []Book {
 	return sortBooks(commonBooks)
 }
 
-// sortBooks sorts the books by Author first and then title
 func sortBooks(books []Book) []Book {
+	sort.Sort(byAuthorThenTitle(books))
+	return books
+}
+
+// sortBooks sorts the books by Author first and then title
+func sortBooksAlt(books []Book) []Book {
 	sort.Slice(books, func(i, j int) bool {
 		if books[i].Author != books[j].Author {
 			return books[i].Author < books[j].Author
