a1e762f7601b80c0e56e978853fc1ffb1968fcf7
abhayk <developer.ak23@gmail.com>
2025-06-16 02:07:21 +0530

    added comments on struct tags in struct_marshalling.go

--- talkingtogolang/struct_marshalling.go
+++ talkingtogolang/struct_marshalling.go
@@ -6,7 +6,22 @@ import (
 )
 
 type Hello struct {
+	// You can specify metadata on fields of structs using what is called a tag line. This tag line is used for
+	// different things. One common use is to format the output of the fields in JSON. That tag line can also be used to
+	// format data for persistence to database, for example: to write a tag line by adding a specific directive after
+	// the field’s type, using backquotes, as shown below.
 	Message string `json:"hellooo"`
+
+	// what it says is:
+	// Message string - defines a struct field named "Message" of type string
+	// `json:"hellooo"` - is a struct tag that tells the JSON encoder/decoder to use "hellooo" as the JSON key name instead of "Message"
+	// So when you marshal this struct to JSON, instead of getting:
+	// {"Message": "Hello World"}
+	// you will get:
+	// {"hellooo": "Hello World"}
+
+	// The struct tag allows you to customize how Go struct fields are represented in JSON (or other formats), enabling
+	// you to use different naming conventions or map to existing JSON schemas that don't match your Go field names
 }
 
 func ReadingMarshalledStruct() {
