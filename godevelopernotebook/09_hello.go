1ec0ab2749c86c21944df3e6e076c719cf392bb1
abhayk <developer.ak23@gmail.com>
2025-06-24 06:50:43 +0530

    HelloWorld via Structs

--- godevelopernotebook/09_hello.go
+++ godevelopernotebook/09_hello.go
@@ -2,17 +2,22 @@ package main
 
 import . "fmt"
 
+// HelloWorld defines a struct (class) with no members in it
 type HelloWorld struct {
 }
 
+// String() - method is being defined on the struct HelloWorld
 func (h HelloWorld) String() string {
 	return "Hello World!"
 }
 
+// Message - is another struct being defined which contains an anonymous field of type HelloWorld
 type Message struct {
 	HelloWorld
 }
 
+// main invokes String() on the Message which calls String() on the embedded type HelloWorld. It can also be invoked directly
+// on the Message type. Line# 24 and 25 are essentially the same.
 func main() {
 	m := Message{}
 	Println("via struct: ", m.HelloWorld.String())
